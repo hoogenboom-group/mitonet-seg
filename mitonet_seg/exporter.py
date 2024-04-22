372e7691dfbea5f6151c40cedabbc8b263bbdd49
Arent Kievits <ajkievits@hpc29.tudelft.net>
2024-04-22 18:52:20 +0200

    Refactor code

---
+++ mitonet_seg/exporter.py
@@ -0,0 +1,58 @@
+import webknossos as wk
+from tempfile import TemporaryDirectory
+
+def local_export(dataset, layer_name, mag, dtype_per_layer="uint16"):
+    """Local export of segmentation layer to WebKnossos (to existing dataset)"""
+    # Make new "segmentation" layer
+    segmentation_layer = dataset.add_layer(
+            layer_name, 
+            wk.SEGMENTATION_CATEGORY,
+            dtype_per_layer=dtype_per_layer,
+            compressed=True,
+            largest_segment_id=None)
+    mag = segmentation_layer.add_mag(MAG, compress=True)
+    
+    # Add "Mag" and write segmentation data to "Mag"
+    mag.write(data=mito_labels,
+              absolute_offset=layer_bbox.topleft)
+    segmentation_layer.refresh_largest_segment_id()
+
+    # Downsample segmentation
+    segmentation_layer.downsample(coarsest_mag=wk.Mag("128-128-1"),
+                                  sampling_mode="constant_z")
+
+def remote_export(url, token, new_dataset_name, layer_name, voxel_size, mag, 
+                  bbox, seg, layers_2_link, dtype_per_layer="uint16", downsample=False):
+    """Remote export of segmentation layer to WebKnossos (creates a new dataset)"""
+    with wk.webknossos_context(
+        url=url,
+        token=token,
+    ):
+        with TemporaryDirectory() as tempdir:
+            new_dataset = wk.Dataset(
+                dataset_path=tempdir,
+                name=new_dataset_name,
+                voxel_size=voxel_size
+            )
+            warped_layer = new_dataset.add_layer(
+                layer_name,
+                wk.SEGMENTATION_CATEGORY,
+                dtype_per_layer=dtype_per_layer,
+                compressed=True
+                )
+            warped_layer.bounding_box = bbox.align_with_mag(MAG) # Highest MAG
+            warped_layer.add_mag(MAG, compress=True).write(seg)
+            
+            # Downsample segmentation
+            if downsample:  
+                warped_layer.downsample(
+                    coarsest_mag=wk.Mag("128-128-1"),
+                    sampling_mode="constant_z"
+                    )
+                
+            # Upload
+            remote_ds = new_dataset.upload(
+                layers_to_link=layers_2_link,
+            )
+    return remote_ds
+        
---
+++ mitonet_seg/importer.py
@@ -0,0 +1,68 @@
+import math
+
+# Data import
+import webknossos as wk
+import numpy as np
+
+def import_wk_dataset_remote(TOKEN, url, dataset_name, organization_id, MAG, layer="color") -> None:
+    # use the context to get access to your group
+    with wk.webknossos_context(
+        token=TOKEN,
+        url=url
+    ):
+
+    # open remote dataset with dataset name, organization id and WebKnossos url 
+        dataset = wk.Dataset.open_remote(
+                dataset_name_or_url = dataset_name,
+                organization_id = organization_id,
+                webknossos_url = url)
+        voxel_size = dataset.voxel_size
+
+        EM = dataset.get_layer(layer) # Layer
+        mag_view = EM.get_mag(MAG) # MagView
+        
+    # return data, voxel size
+    return dataset, mag_view, voxel_size
+
+def import_wk_dataset_local(dir_path, MAG, layer="color") -> None:
+    # open local dataset in given directory 
+    dataset = wk.Dataset.open(
+        dataset_path = dir_path)
+    voxel_size = dataset.voxel_size
+
+    EM = dataset.get_layer(layer) # Layer
+    mag_view = EM.get_mag(MAG) # MagView
+        
+    # return data, voxel size
+    return dataset, mag_view, voxel_size
+
+def read_data(layer_bbox, MAG, bboxes, mag_view): 
+    # Infer data set dimensions (in desired mag)
+    dim = layer_bbox.in_mag(MAG).size
+
+    # Allocate memory
+    data = np.zeros((1, dim.x, dim.y, dim.z), dtype=np.uint8)
+
+    # Read data from remote in chunks
+    # By looping through bboxes
+    x_offset = bboxes[0].topleft[0]
+    y_offset = bboxes[0].topleft[1]
+    for bbox_small in tqdm(bboxes,
+                        desc="Reading data from bboxes",
+                        total=len(bboxes),
+                        unit="bbox"):
+        # Fill slice in array with data from smaller bbox
+        view_small = mag_view.get_view(absolute_offset=bbox_small.topleft, 
+                                    size=bbox_small.size)
+        x_start = bbox_small.in_mag(MAG).topleft.x - x_offset
+        x_end = x_start + bbox_small.in_mag(MAG).size.x
+        y_start = bbox_small.in_mag(MAG).topleft.y - y_offset
+        y_end = y_start + bbox_small.in_mag(MAG).size.y
+        
+        data[:, 
+            x_start:x_end,
+            y_start:y_end,
+            :] = view_small.read()
+    
+    # return data
+    return data
