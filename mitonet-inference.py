fb2a4cb0efd1be6072f6748bb2f884787241f6ae
Arent Kievits <ajkievits@hpc29.tudelft.net>
2024-04-25 13:49:57 +0200

    Update format

--- mitonet-inference.py
+++ mitonet-inference.py
@@ -17,8 +17,9 @@ DATASET_NAME = "20230626_RP_serial_warped_full_1x_mito_seg" # Dataset name as in
 EM_LAYER = "color"
 CONFIG = os.path.abspath("configs/MitoNet_v1.yaml") # MitoNet model configuration file
 REMOTE = False # Set to "True" if importing data remotely (without filesystem mount)
-USE_CPU = False # Use GPU, seting to "True" falls back to CPU (computations are much slower)
+USE_CPU = False # Use GPU, setting to "True" falls back to CPU (computations are much slower)
 DOWNSAMPLE = True # Set to "True" to automatically downsample segmentations
+DTYPE_SEG = 'uint16' # Datatype, standard is uint16
 
 # Parameters that should likely be set once
 MAG_X, MAG_Y, MAG_Z = 4, 4, 1 # Magnification level (x, y, z) in WebKnossos to be used for segmentation. Default is (4, 4, 1)
@@ -31,6 +32,7 @@ def _main():
     MAG = wk.Mag(f"{MAG_X}-{MAG_Y}-{MAG_Z}") # Set magnification for WebKnossos
     if REMOTE:
         assert TOKEN, f"No WebKnossos token supplied, please generate from https://webknossos.tnw.tudelft.nl/auth/token"
+        # Fetch dataset remotely
         dataset, mag_view, voxelsize = importer.import_wk_dataset_remote(TOKEN, 
                                                                          URL, 
                                                                          DATASET_NAME,
@@ -39,14 +41,14 @@ def _main():
                                                                          EM_LAYER)    
     else:
         DIR_PATH = pathlib.Path("{BASE_DIR}/webknossos/binaryData/{ORGANIZATION_ID}/{DATASET_NAME}")
-        # Import full data set at required zoom level
+        # Fetch dataset locally
         dataset, mag_view, voxelsize = importer.import_wk_dataset_local(DIR_PATH,
                                                                         MAG,
                                                                         EM_LAYER)
     # Bounding box
     layer_bbox = dataset.get_layer(EM_LAYER).bounding_box   
     
-    # Define chunks if array size exceeds vram
+    # Define chunks because array size may exceed vram
     view = mag_view.get_view(offset=layer_bbox.in_mag(MAG).topleft, size=layer_bbox.in_mag(MAG).size)
     bboxes = define_bbox_chunks(view, mag=MAG, bbox_size=10000)
     
@@ -62,12 +64,11 @@ def _main():
         new_dataset_name = DATASET_NAME + '_mito_seg'
         new_layer_name = EM_LAYER + "_MitoNet"
         exporter.remote_export(url=URL, token=TOKEN, new_dataset_name=new_dataset_name, layer_name=new_layer_name, voxel_size=voxelsize, mag=MAG, 
-                               bbox=layer_bbox, seg=mito_labels, layers_2_link=layers_2_link, dtype_per_layer="uint16", downsample=DOWNSAMPLE)
+                               bbox=layer_bbox, seg=mito_labels, layers_2_link=layers_2_link, dtype_per_layer=DTYPE_SEG, downsample=DOWNSAMPLE)
     else:
-        exporter.local_export(dataset=dataset, layer_name=new_layer_name, mag=MAG, dtype_per_layer="uint16")
+        exporter.local_export(dataset=dataset, layer_name=new_layer_name, mag=MAG, dtype_per_layer=DTYPE_SEG)
     
     
-
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
